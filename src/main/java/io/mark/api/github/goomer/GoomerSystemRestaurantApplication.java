a02527f7e505af24ad413fb91341d80aaad1a0db
immark007 <immark5507@gmail.com>
2025-03-15 16:10:01 -0300

    feat: initial project

---
+++ src/main/java/io/mark/api/github/goomer/GoomerSystemRestaurantApplication.java
@@ -0,0 +1,13 @@
+package io.mark.api.github.goomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoomerSystemRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoomerSystemRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/mark/api/github/goomer/GoomerSystemRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mark.api.github.goomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoomerSystemRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
