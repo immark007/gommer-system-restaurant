a46514f0e22e33caa0d440e1b51c66172141cd7a
immark007 <immark5507@gmail.com>
2025-03-28 12:30:49 -0300

    feat: criando um generic para retornar a URI no location

---
+++ src/main/java/io/mark/api/github/goomer/controller/GenericController.java
@@ -0,0 +1,12 @@
+package io.mark.api.github.goomer.controller;
+
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.UUID;
+
+public interface GenericController {
+    default URI gerarHeaderLocation(UUID id) {
+        return ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(id).toUri();
+    }
+}
