925b3957f56779f0ae83780b97243ff1b8de6af4
immark007 <immark5507@gmail.com>
2025-03-28 12:27:59 -0300

    feat: criando dto para criação e autenticação de um usuário

--- src/main/java/io/mark/api/github/goomer/dto/CreateUserDTO.java
+++ src/main/java/io/mark/api/github/goomer/dto/CreateUserDTO.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.UUID;
 
 public record CreateUserDTO(
         @Email(message = "formato de email invalido")
