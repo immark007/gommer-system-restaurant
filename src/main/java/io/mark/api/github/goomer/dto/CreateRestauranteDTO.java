c50c8e5fa8462e40fe881ed2d76af14831da8153
immark007 <immark5507@gmail.com>
2025-03-28 12:27:50 -0300

    feat: criando dto para criação e autenticação de um usuário

---
+++ src/main/java/io/mark/api/github/goomer/dto/CreateRestauranteDTO.java
@@ -0,0 +1,23 @@
+package io.mark.api.github.goomer.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+import java.util.UUID;
+
+public record CreateRestauranteDTO(
+        UUID id,
+        @NotBlank(message = "O campo não pode ser vázio")
+        String fotoURL,
+        @NotBlank(message = "O campo não pode ser vázio")
+        String nome,
+        @NotNull(message = "O campo não pode ser nullo")
+        EnderecoDTO endereco,
+        @NotBlank(message = "O campo não pode ser vázio")
+        String horarioFuncionamento,
+        @NotNull(message = "O campo não pode ser vázio")
+        UUID usuarioId,
+        List<ProdutoDTO> produtos
+) {
+}
