4cc4fde04c8e4f324ff4852624a684b32c456cb1
immark007 <immark5507@gmail.com>
2025-03-29 12:39:20 -0300

    fix: tirando dependencia de classes

--- src/main/java/io/mark/api/github/goomer/controller/ClientController.java
+++ src/main/java/io/mark/api/github/goomer/controller/ClientController.java
@@ -4,7 +4,6 @@ import io.mark.api.github.goomer.model.Client;
 import io.mark.api.github.goomer.service.ClientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
